6f464afe51658012740b512c163ab51470b66f85
cvwang <charlesvwang@gmail.com>
2014-10-05 00:09:53 -0400

    Initial Commit

---
+++ iFinance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iFinance
+//
+//  Created by Charles Wang on 10/5/14.
+//  Copyright (c) 2014 Charles Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
